a22064aec4ff57fd129584753f0371737a54b14d
NOTuglyduckling <osearle65@gmail.com>
2023-11-02 13:59:07 +0100

    FINI TP7

--- TP7/EX2.c
+++ TP7/EX2.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+
+void Trier(int Tab[], int NbElts);
+void Aff(int Tab[], int NbElts);
+int* TrierPtr(int Tab[], int NbElts);
+void AffPtr(int* TabP, int NbElts);
 
 int main(void) {
-    // Calculate the number of bytes for approximately 8GB of RAM
-    uint64_t bytesToAllocate = (uint64_t)20 * 1024 * 1024 * 1024; // 8GB in bytes
-
-    // Allocate a large block of memory
-    char* largeMemoryBlock = (char*)malloc(bytesToAllocate);
-
-    if (largeMemoryBlock == NULL) {
-        printf("Memory allocation failed.\n");
-        return 1;
-    }
-
-    // Initialize the allocated memory (optional)
-    for (uint64_t i = 0; i < bytesToAllocate; i++) {
-        largeMemoryBlock[i] = 0;
-    }
-
-    // Simulate using the allocated memory (e.g., reading/writing)
-    // ...
-
-    // Free the allocated memory when done
-    free(largeMemoryBlock);
-
+    int Tab[5] = {3, 2, 7, 4, 0};
+    int* PTab = TrierPtr(Tab, 5);
+    Trier(Tab, 5);
+    AffPtr(PTab, 5);
+    Aff(Tab, 5);
+    
+    free(PTab); // Don't forget to free the memory allocated by TrierPtr
     return 0;
+}
+
+void Trier(int Tab[], int NbElts) {
+    int swapped;
+    do {
+        swapped = 0;
+        for (int i = 1; i < NbElts; i++) {
+            if (Tab[i - 1] > Tab[i]) {
+                int temp = Tab[i - 1];
+                Tab[i - 1] = Tab[i];
+                Tab[i] = temp;
+                swapped = 1;
+            }
+        }
+    } while (swapped);
+}
+
+void Aff(int Tab[], int NbElts) {
+    for (int i = 0; i < NbElts; i++) {
+        printf("%d ", Tab[i]);
+    }
+    printf("\n");
+}
+
+int* TrierPtr(int Tab[], int NbElts) {
+    int* PTab = malloc(NbElts * sizeof(int));
+    if (PTab == NULL) {
+        printf("Erreur d'allocation memoire\n");
+        return NULL;
+    }
+    for (int i = 0; i < NbElts; i++) {
+        PTab[i] = Tab[i];
+    }
+    Trier(PTab, NbElts);
+    return PTab;
+}
+
+void AffPtr(int* TabP, int NbElts) {
+    for (int i = 0; i < NbElts; i++) {
+        printf("%d ", TabP[i]); // Use %d to print integers, not %p
+    }
+    printf("\n");
 }
